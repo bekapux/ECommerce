1619134c4434b15f8f1f41f8f70500ae43eab3e2
Beka Pukhashvili <beka.pukhashvili@gmail.com>
2022-03-09 06:06:05 -0800

    added routing

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,7 +1,8 @@
-import { SetMealRounded, ShoppingCart } from "@mui/icons-material";
+import { ShoppingCart } from "@mui/icons-material";
 import {
   AppBar,
   Badge,
+  Box,
   IconButton,
   List,
   ListItem,
@@ -27,48 +28,62 @@ const rightLinks = [
   { title: "register", path: "/register" },
 ];
 
+const navStyles = {
+  textDecoration: "none",
+  color: "inherit",
+  typography: "h6",
+  "&:hover": {
+    color: "grey.500",
+  },
+  "&.active": {
+    color: "text.secondary",
+  },
+};
+
 const Header = ({ darkMode, handleThemeChange }: Props) => {
   return (
     <AppBar position="static" sx={{ mb: 4 }}>
-      <Toolbar>
-        <Typography
-          variant="h6"
-          component={NavLink}
-          to={"/"}
-          sx={{ color: "inherit", textDecoration: "none" }}
-        >
-          E-Commerce
-        </Typography>
-        <Switch checked={darkMode} onChange={handleThemeChange}></Switch>
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Box display="flex" alignItems="center">
+          <Typography
+            exact
+            variant="h6"
+            component={NavLink}
+            to={"/"}
+            sx={navStyles}
+          >
+            E-Commerce
+          </Typography>
+          <Switch checked={darkMode} onChange={handleThemeChange}></Switch>
+        </Box>
+
         <List sx={{ display: "flex" }}>
           {midLinks.map(({ title, path }) => (
-            <ListItem
-              component={NavLink}
-              to={path}
-              key={path}
-              sx={{ color: "inherit", typography: "h6" }}
-            >
+            <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
               {title.toUpperCase()}
             </ListItem>
           ))}
         </List>
-        <IconButton size="large" sx={{ color: "inherit" }}>
-          <Badge badgeContent={4} color="secondary">
-            <ShoppingCart />
-          </Badge>
-        </IconButton>
-        <List sx={{ display: "flex" }}>
-          {rightLinks.map(({ title, path }) => (
-            <ListItem
-              component={NavLink}
-              to={path}
-              key={path}
-              sx={{ color: "inherit", typography: "h6" }}
-            >
-              {title.toUpperCase()}
-            </ListItem>
-          ))}
-        </List>
+        <Box display="flex" alignItems="center">
+          <IconButton size="large" sx={{ color: "inherit" }}>
+            <Badge badgeContent={4} color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+          <List sx={{ display: "flex" }}>
+            {rightLinks.map(({ title, path }) => (
+              <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Toolbar>
     </AppBar>
   );
--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -8,7 +8,7 @@ import {
   CardMedia,
   Typography,
 } from "@mui/material";
-import { bgcolor } from "@mui/system";
+import { Link } from "react-router-dom";
 import { Product } from "../../app/models/product";
 
 interface Props {
@@ -47,7 +47,9 @@ const ProductCard = ({ product }: Props) => {
       </CardContent>
       <CardActions>
         <Button size="small">Add to cart</Button>
-        <Button size="small">View</Button>
+        <Button component={Link} to={`/catalog/${product.id}`} size="small">
+          View
+        </Button>
       </CardActions>
     </Card>
   );
