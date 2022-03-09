471702cf407caf28ab5da5522c4264c5c2b95432
Beka Pukhashvili <beka.pukhashvili@gmail.com>
2022-03-09 02:02:56 -0800

    added router

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,18 +1,74 @@
-import { SetMealRounded } from "@mui/icons-material";
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { SetMealRounded, ShoppingCart } from "@mui/icons-material";
+import {
+  AppBar,
+  Badge,
+  IconButton,
+  List,
+  ListItem,
+  Switch,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 interface Props {
   darkMode: boolean;
   handleThemeChange: () => void;
 }
 
+const midLinks = [
+  { title: "catalog", path: "/catalog" },
+  { title: "about", path: "/about" },
+  { title: "contact", path: "/contact" },
+];
+const rightLinks = [
+  { title: "login", path: "/login" },
+  { title: "register", path: "/register" },
+];
+
 const Header = ({ darkMode, handleThemeChange }: Props) => {
   return (
     <AppBar position="static" sx={{ mb: 4 }}>
       <Toolbar>
-        <Typography variant="h6">E-Commerce</Typography>
+        <Typography
+          variant="h6"
+          component={NavLink}
+          to={"/"}
+          sx={{ color: "inherit", textDecoration: "none" }}
+        >
+          E-Commerce
+        </Typography>
         <Switch checked={darkMode} onChange={handleThemeChange}></Switch>
+        <List sx={{ display: "flex" }}>
+          {midLinks.map(({ title, path }) => (
+            <ListItem
+              component={NavLink}
+              to={path}
+              key={path}
+              sx={{ color: "inherit", typography: "h6" }}
+            >
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
+        <IconButton size="large" sx={{ color: "inherit" }}>
+          <Badge badgeContent={4} color="secondary">
+            <ShoppingCart />
+          </Badge>
+        </IconButton>
+        <List sx={{ display: "flex" }}>
+          {rightLinks.map(({ title, path }) => (
+            <ListItem
+              component={NavLink}
+              to={path}
+              key={path}
+              sx={{ color: "inherit", typography: "h6" }}
+            >
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
       </Toolbar>
     </AppBar>
   );
---
+++ client/src/features/catalog/ProductDetails.tsx
@@ -0,0 +1,8 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const ProductDetails = () => {
+  return <Typography variant="h2">Product Details Page</Typography>;
+};
+
+export default ProductDetails;
