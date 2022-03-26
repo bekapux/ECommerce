af422b01612a65e6572f5f450a24fbc7ebb8770f
Beka Pukhashvili <beka.pukhashvili@gmail.com>
2022-03-26 19:15:41 +0400

    Learning redux

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,8 +1,39 @@
-import { Typography } from "@mui/material";
-import React from "react";
+import { Button, ButtonGroup, Typography } from "@mui/material";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+import { decrement, increment } from "./counterSlice";
 
 const ContactPage = () => {
-  return <Typography variant="h2">Contact Page</Typography>;
+  const dispatch = useAppDispatch();
+  const { data, title } = useAppSelector((state) => state.counter);
+  return (
+    <>
+      <Typography variant="h2">{title}</Typography>
+      <Typography variant="h5">The data is: {data}</Typography>
+      <ButtonGroup>
+        <Button
+          variant="contained"
+          color="error"
+          onClick={() => dispatch(decrement(1))}
+        >
+          Decrement
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => dispatch(increment(1))}
+        >
+          Increment
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => dispatch(increment(5))}
+        >
+          Increment 5
+        </Button>
+      </ButtonGroup>
+    </>
+  );
 };
 
 export default ContactPage;
